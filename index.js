cd24e81ba29a05bb2a06bdfa728eab61c4aae0f2
herve <hdjomo1@gmail.com>
2023-12-07 21:59:19 +0100

    calculator using css and html

---
+++ index.js
@@ -0,0 +1,4 @@
+var btn = document.getElementsByClassName("btn")
+var oper = document.getElementsByClassName("oper")
+var clear = document.getElementsByClassName("clear")
+var ans = document.getElementsByClassName("ans")
